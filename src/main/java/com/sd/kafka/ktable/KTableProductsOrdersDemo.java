d063796d0759838bd2d4beadf4d8a653ceb86c9f
@SreenuDoosari <sreenu.doosari@gmail.com>
2025-10-29 23:02:20 +0100

    working ktable example

--- src/main/java/com/sd/kafka/ktable/KTableProductsOrdersDemo.java
+++ src/main/java/com/sd/kafka/ktable/KTableProductsOrdersDemo.java
@@ -1,31 +1,43 @@
 package com.sd.kafka.ktable;
 
-import com.sd.kafka.streams.basic.Widget;
-import com.sd.kafka.streams.basic.WidgetSerde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.state.KeyValueStore;
-
-import java.time.Duration;
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
 
-import java.util.Properties;
+/*
+Input data
 
+Topic: products
+
+Key=P1, Value=Phone
+Key=P2, Value=TV
+
+Topic: orders
+Key=P1, Value=Order#101
+Key=P2, Value=Order#102
+
+Output: enriched-orders
+Key=P1, Value=Order=Order#101, Product=Phone
+Key=P2, Value=Order=Order#102, Product=TV
+
+Concept	Demonstrated
+KTable	Keeps latest product per key
+KStream	Represents continuous order events
+Join	Stream-table join for enrichment
+Local state	Products table stored as a StateStore
+Output	New topic with enriched results
+
+ */
 public class KTableProductsOrdersDemo {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable-demo");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
@@ -33,10 +45,10 @@ public class KTableProductsOrdersDemo {
 
         // KTable for product reference data
         KTable<String, String> products = builder.table("products");
-
+        System.out.println("got products");
         // KStream for order events
         KStream<String, String> orders = builder.stream("orders");
-
+        System.out.println("got orders");
         // Join stream with table
         KStream<String, String> enriched = orders.join(
             products,
@@ -48,5 +60,6 @@ public class KTableProductsOrdersDemo {
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
+        System.out.println("finished");
     }
 }
